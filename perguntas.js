c1504d12bdbc6cf4b88f4a60e0457cce76db3b3b
ceboliinha09 <00001123444985sp@al.educacao.sp.gov.br>
2024-11-24 01:18:34 -0300

    envio do arquivo

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasília'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua Inglesa',
+    'Como se diz tchau em Inglês?',
+    'Tchau em Inglês é Goodbye'
+)
+
+criaCartao(
+    'HIstória',
+    'Quem foi o politico considerado "Pai dos pobres"?',
+    'Getúlio Vargas, pois promoveu importantes reformas trabalhistas e sociais'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a fórmula da área de um círculo?',
+    'A área de um círculo é dada por A = π * r², onde "r" é o raio do círculo'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior rio do mundo?',
+    'O maior rio do mundo é o Rio Amazonas, em termos de volume de água'
+)
+
+criaCartao(
+    'História',
+    'O que foi a Guerra Fria?',
+    'A Guerra Fria foi um período de tensão política e militar entre os Estados Unidos e a União Soviética, sem conflitos diretos, mas com várias guerras por procuração'
+)
+
+criaCartao(
+    'Geografia',
+    'Quantos continentes existem no mundo?',
+    'Existem sete continentes no mundo: Ásia, África, América do Norte, América do Sul, Antártica, Europa e Oceania'
+)
